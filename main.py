1dc439ba4bb305de6a7edc68450d5a5785c7cb99
Dmitry Popov <ant1h4t3rz@gmail.com>
2021-09-23 21:58:54 +0900

    Added initial code and .gitignore

---
+++ main.py
@@ -0,0 +1,41 @@
+def get_recipes_dict(filename, mode, encode):
+    recipe_dict = dict()
+    dish = str()
+    ingredient = str()
+    quantity = str()
+    amount = str()
+    measure = str()
+    buffer_list = list()
+    with open(filename, mode, encoding=encode) as file:
+        for line in file:
+            dish = line.strip()
+            amount = int(file.readline())
+            buffer_list = list()
+            for item in range(amount):
+                ingredient, quantity, measure = file.readline().split('|')
+                buffer_list.append(
+                    {'ingredient_name': ingredient.strip(), 'quantity': int(quantity), 'measure': measure.strip()}
+                )
+            recipe_dict[dish] = buffer_list
+            file.readline()
+    return recipe_dict
+
+
+def get_shop_list_by_dishes(cook_dict, dishes, person_count):
+    temp_list = list()
+    for dish_name in dishes:
+        for dish, ingredient in cook_dict.items():
+            if dish == dish_name:
+                pass
+
+    return 0
+
+
+path = 'recipes.txt'
+encoding_type = 'utf-8'
+mode = 'r'
+cook_book = get_recipes_dict(path, mode, encoding_type)
+
+
+
+
